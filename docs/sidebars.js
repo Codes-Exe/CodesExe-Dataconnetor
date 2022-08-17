3c670eaa9528eeb153d4a9df291034aa5c445811
Erçetin Güler <ercetinguler@hotmail.com>
2022-08-17 06:05:10 +0300

    Initial commit

---
+++ docs/sidebars.js
@@ -0,0 +1,21 @@
+module.exports = {
+  docs: [
+    {
+      type: 'category',
+      label: 'Docusaurus Tutorial',
+      items: [
+        'getting-started',
+        'parameters',
+        'binance',
+        'coinbase',
+        'coinmarketcap',
+        'github',
+        'google-analytics',
+        'pse-lookup',
+        'stripe',
+        'yahoo-finance',
+        'youtube'
+      ],
+    },
+  ],
+};
